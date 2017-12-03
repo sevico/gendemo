85c525cadbdf8aeacb3de56144a902197bb61585
swkhack <swkhack@gmail.com>
2017-12-04 01:16:07 +0800

    init

---
+++ demo.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+
+
+fs.mkdirSync("./" + dirName)
+process.chdir("./" + dirName)
+fs.mkdirSync('css')
+fs.mkdirSync('js')
+
+var index=`
+ <!DOCTYPE>
+<title>Hello</title>
+<h1>Hi</h1>`
+var css = `h1{color: red;}`
+var main_js = `
+var string = "Hello World"
+alert(string)
+`
+fs.writeFileSync("./index.html", index)
+fs.writeFileSync("css/style.css", css)
+fs.writeFileSync("./js/main.js", main_js)
+
+process.exit(0)
