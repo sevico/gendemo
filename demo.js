169e73fd99576c84142b91bcbb7717cae1adac4b
swkhack <swkhack@gmail.com>
2017-12-04 08:26:52 +0800

    directory exist

--- demo.js
+++ demo.js
@@ -3,23 +3,28 @@
 var fs = require('fs')
 var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
 
+if (fs.existsSync(dirName)) {
+    console.log("directory exist!")
+    process.exit(1)
 
-fs.mkdirSync("./" + dirName)
-process.chdir("./" + dirName)
-fs.mkdirSync('css')
-fs.mkdirSync('js')
+} else {
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName)
+    fs.mkdirSync('css')
+    fs.mkdirSync('js')
 
-var index=`
+    var index = `
  <!DOCTYPE>
 <title>Hello</title>
 <h1>Hi</h1>`
-var css = `h1{color: red;}`
-var main_js = `
+    var css = `h1{color: red;}`
+    var main_js = `
 var string = "Hello World"
 alert(string)
 `
-fs.writeFileSync("./index.html", index)
-fs.writeFileSync("css/style.css", css)
-fs.writeFileSync("./js/main.js", main_js)
+    fs.writeFileSync("./index.html", index)
+    fs.writeFileSync("css/style.css", css)
+    fs.writeFileSync("./js/main.js", main_js)
 
-process.exit(0)
+    process.exit(0)
+}
